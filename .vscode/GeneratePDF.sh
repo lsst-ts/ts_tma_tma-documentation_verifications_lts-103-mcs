d6d5b87c96f0ef0541e84922a879b7a5e2d2fc07
julen_port <julen.garcia@tekniker.es>
2023-09-04 11:57:17 +0200

    build: add .gitlab-ci.yml

--- .vscode/GeneratePDF.sh
+++ .vscode/GeneratePDF.sh
@@ -1,7 +1,3 @@
-docker run --rm \
---volume "$(pwd):/data" \
---user $(id -u):$(id -g) \
-gitlab.tekniker.es:5001/aut/personal/jgsaenz/pandoc-tekniker-pdf-template-dockerimage/pandoc-tekniker-template:latest \
 README.md \
 -o LTS_103_Validation.pdf \
 --metadata=title:'LTS - 103 Validation for MCS' \
