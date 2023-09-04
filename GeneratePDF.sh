c6e59a6f20efdbe9581c6484f2d4fb2c3f41c0bb
julen_port <julen.garcia@tekniker.es>
2023-09-04 12:30:25 +0200

    Revert "Auxiliary commit to revert individual files from bb5d60f1c3f09fbf3882f0bdaec32535706d8cc4"
    This reverts commit 639ca70cf584003421081c84895995530eb5fbec.

--- GeneratePDF.sh
+++ GeneratePDF.sh
@@ -5,4 +5,9 @@ docker run --rm \
 --user $(id -u):$(id -g) \
 gitlab.tekniker.es:5001/aut/personal/jgsaenz/pandoc-tekniker-pdf-template-dockerimage/pandoc-tekniker-template:latest \
 README.md \
--o LTS_103_Validation.pdf
+-o LTS_103_Validation.pdf \
+--metadata=title:'LTS - 103 Validation for MCS' \
+--metadata=subtitle: "$1" \
+--metadata=author:'Julen Garcia' \
+--metadata=date: "$2" \
+--metadata=lang:'en-EN'
