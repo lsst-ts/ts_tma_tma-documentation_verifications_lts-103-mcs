d0f3240c9b09ad18eee291e4216632375473334d
julen_port <julen.garcia@tekniker.es>
2023-09-04 12:31:07 +0200

    build: add .gitlab-ci.yml

--- GeneratePDF.sh
+++ GeneratePDF.sh
@@ -7,7 +7,3 @@ gitlab.tekniker.es:5001/aut/personal/jgsaenz/pandoc-tekniker-pdf-template-docker
 README.md \
 -o LTS_103_Validation.pdf \
 --metadata=title:'LTS - 103 Validation for MCS' \
---metadata=subtitle: "$1" \
---metadata=author:'Julen Garcia' \
---metadata=date: "$2" \
---metadata=lang:'en-EN'
